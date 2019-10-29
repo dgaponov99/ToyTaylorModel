ed49b852df761645b76b4d20a5719ab81f758802
dmitry <dgaponov99@mail.ru>
2019-10-29 22:34:08 +0300

    update properties

--- MultiPolynomial.m
+++ MultiPolynomial.m
@@ -2,22 +2,19 @@ classdef MultiPolynomial
 %    Класс полинома многих переменных в разложении по степеням
     
     properties (Access = private)
-        varNumber;
-        degree;
+        dimX;
         coeffs;
         x0;
     end
     
     methods
-        function obj = MultiPolynomial(varNumber, degree, positions, values, x0)
-            %   varNumber - число переменных полинома
-            %   degree - степень полинома (сумма показателей при степенях не превосходит это число)
+        function obj = MultiPolynomial(positions, values, x0)
+            %   dimX - размерность пространства
             %   positions - массив ячеек с показателями слогаемого (например: {[2 0] [1 1] [0 2]})
             %   values - значения коэффициентов соответствующих слогаемых
             %(например: [1 2 1])
-            %   x0 - точка разложения полинома (опционально)
-            obj.varNumber = varNumber;
-            obj.degree = degree;
+            %   x0 - (опционально) точка разложения полинома
+            obj.dimX = length(positions{1});
             obj.coeffs = containers.Map(conv2str(positions), values);
             if nargin > 4
                 obj.x0 = x0;
@@ -33,7 +30,7 @@ classdef MultiPolynomial
             for i = 1:length(strKeys)
                 term = self.coeffs(strKeys{i});
                 quantitys = str2num(strKeys{i});
-                for j = 1:self.varNumber
+                for j = 1:self.dimX
                     term = term*(x(j) - self.x0(j))^(quantitys(j));
                 end
                 value = value + term;
@@ -47,7 +44,7 @@ classdef MultiPolynomial
             for i = 1:length(strKeys)
                 term = num2str(self.coeffs(strKeys{i}));
                 quantitys = str2num(strKeys{i});
-                for j = 1:self.varNumber
+                for j = 1:self.dimX
                     q = quantitys(j);
                     if q ~= 0
                         term = strcat(term, "(x", num2str(j));
